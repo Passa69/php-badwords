f7e1f45f17c3c3c6b20535312a29192360e631b5
tommaso <passarotto13@gmail.com>
2021-12-20 09:58:42 +0100

    aggiunti elementi per scrittura

--- index.php
+++ index.php
@@ -5,11 +5,22 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    <?php
+        $x = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente, laudantium? Adipisci nihil debitis, at magni corrupti quaerat numquam voluptatem ad iusto tenetur eligendi omnis dolorum molestias ipsum saepe perspiciatis! Repellendus.";
+    ?>
 </head>
 <body>
-    <?php 
-        echo $x = 'ciao Tommaso'; 
+    <p>
+        <?php 
+            echo $x; 
+        ?>
+    </p>
+
+    <p>
+        <?php
         echo strlen($x);
-    ?>
+        ?>
+    </p>
 </body>
 </html>
